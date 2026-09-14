bb26ff8350204b4ae5ecc1112386d6673e395db8
kennethliew <66197803+kennethljx@users.noreply.github.com>
2026-09-14 09:16:08 +0800

    ex4

--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,7 +3,15 @@ import { ref } from 'vue'
 const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
 // TODO: Add Code Here
+function deleteItem (index) {
+    items.value.splice(index,1)
+}
 
+const newItem = ref('');
+function addItem(item) {
+    items.value.push(item)
+    newItem.value = ""
+}
 
 </script>
 
@@ -11,6 +19,15 @@ const items = ref(['keyboard', 'mouse', 'iPhone', 'macbook', 'adapter'])
 
     <h2>Shopping Cart</h2>
     <!-- TODO: Add Code Here -->
-
+    <ul>
+        <li v-for="(item, index) in items">
+            {{ item }}
+            <button @click="deleteItem(index)">Delete</button>
+        </li>
+    </ul>
+    <input type="text" v-model.trim="newItem" v-on:keyup="addItem(newItem)">
+    <button @click="addItem(newItem)" >Add!</button>
    
 </template>
+
+<!--  -->
